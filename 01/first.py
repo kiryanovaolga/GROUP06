9d499eb3268d3a4b31018edbd57a813cfc9b4dfd
Suche Ganbaatar <113457666+suche-cz@users.noreply.github.com>
2024-11-10 22:41:51 +0100

    Initial commit

---
+++ 01/first.py
@@ -0,0 +1,14 @@
+vek = input("Zadej věk: ")
+
+try:
+    vek = int(vek)
+
+    if vek > 12 and vek < 20:
+        print("Jsi teenager")
+    else:
+        print("Nejsi teenager")
+except:
+    print("Neplatná hodnota")
+
+
+
