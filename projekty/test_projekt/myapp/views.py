3940a0da6b1d2e3474e8394dea2feb72e2c3aa81
Suche Ganbaatar <113457666+suche-cz@users.noreply.github.com>
2025-01-21 22:05:08 +0100

    PY17 django test_projekt

---
+++ projekty/test_projekt/myapp/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import random
+import datetime as dt
+
+def index_page(request):
+    print(request.method)
+    print(request.path)
+    
+    number = random.randint(1, 100)
+    d = dt.datetime.now()
+
+    return HttpResponse(f'''
+                        <h1>Hello From Django: {number} | {d}</h1>
+                        <img src="https://picsum.photos/200/300">
+                        ''')
+
