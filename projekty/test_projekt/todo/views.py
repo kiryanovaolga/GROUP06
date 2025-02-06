7f811d2737b3c47a6f55a383f6a6438c1cfc0557
Suche Ganbaatar <113457666+suche-cz@users.noreply.github.com>
2025-02-06 21:03:35 +0100

    Task form

--- projekty/test_projekt/todo/views.py
+++ projekty/test_projekt/todo/views.py
@@ -1,16 +1,37 @@
-from django.shortcuts import render
+from django.forms import ModelForm
+from django.shortcuts import render, redirect
 from todo.models import Task
+
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'desc']
 
 
 def index(request):
     return render(request, 'todo/index.html')
 
 def todo_list(request):
-    tasks = Task.objects.all()[0:100]
+    tasks = Task.objects.all().order_by('-id')[0:100]
     return render(request, 'todo/list.html', {'tasks': tasks})
 
 def todo_new(request):
-    return render(request, 'todo/new.html')
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save()
+            print(task, task.id)
+            return redirect('/todo/list/') # todo: opravit
+        else:
+            form = TaskForm(request.POST)
+            return render(request, 'todo/new.html', {'form': form})
+    else:
+        form = TaskForm()
+        return render(request, 'todo/new.html', {'form': form})
+    
+    
 
 def todo_detail(request, number):
-    return render(request, 'todo/detail.html')
+    task = Task.objects.get(id=number)
+    return render(request, 'todo/detail.html', {'task': task})
