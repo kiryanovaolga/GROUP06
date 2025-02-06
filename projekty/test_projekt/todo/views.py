1a2ecd1626369548c38ac9d0e58ed2697a852769
Suche Ganbaatar <113457666+suche-cz@users.noreply.github.com>
2025-02-06 19:26:56 +0100

    PY22

--- projekty/test_projekt/todo/views.py
+++ projekty/test_projekt/todo/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from todo.models import Task
+
 
 def index(request):
     return render(request, 'todo/index.html')
 
 def todo_list(request):
-    return render(request, 'todo/list.html')
+    tasks = Task.objects.all()[0:100]
+    return render(request, 'todo/list.html', {'tasks': tasks})
 
 def todo_new(request):
     return render(request, 'todo/new.html')
